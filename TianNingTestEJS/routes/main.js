90c03dc17b6ef35e12fa945242150ad856e5a497
Cheetn <Cheetianning123@gmail.com>
2026-07-13 01:00:12 +0800

    front end for game

--- TianNingTestEJS/routes/main.js
+++ TianNingTestEJS/routes/main.js
@@ -1,13 +1,30 @@
-// Avan Here, App Journey Starts here!
-// Main page router only for home page
-
 const express = require("express");
 const router = express.Router();
 
+// Home page
 router.get('/', (req, res, next) => {
     res.render("index.ejs", {title: "HOME"});
 });
 
-// Main Page, no need to check for ESP32 yet
+// Game route - Ticket Rail Game
+router.get('/game', (req, res, next) => {
+    res.render("game.ejs", { 
+        title: "Game - Cooking Adventure" 
+    });
+});
+
+// Game Settings route - Control Dashboard
+router.get('/game/settings', (req, res, next) => {
+    res.render("gamesettings.ejs", { 
+        title: "Game Settings" 
+    });
+});
+
+// Alternative: Also support /gamesettings (without /game/ prefix)
+router.get('/gamesettings', (req, res, next) => {
+    res.render("gamesettings.ejs", { 
+        title: "Game Settings" 
+    });
+});
 
 module.exports = router;
