0f89a363211e65b62dcfc97baadc60e4e50b1c6f
Cheetn <Cheetianning123@gmail.com>
2026-07-15 02:35:16 +0800

    added in the new pages and buttons

--- TianNingTestEJS/routes/main.js
+++ TianNingTestEJS/routes/main.js
@@ -12,4 +12,20 @@ router.get('/', (req, res, next) => {
     res.render("index.ejs", {title: "HOME"});
 });
 
+// NEW ROUTES - Add these after the home page route
+// ESP Dashboard page
+router.get('/espdashboard', (req, res) => {
+    res.render("espdashboard.ejs", { title: "ESP Dashboard" });
+});
+
+// Leaderboards page
+router.get('/leaderboards', (req, res) => {
+    res.render("leaderboards.ejs", { title: "Leaderboards" });
+});
+
+// Credits page
+router.get('/credits', (req, res) => {
+    res.render("credits.ejs", { title: "Credits" });
+});
+
 module.exports = router;
